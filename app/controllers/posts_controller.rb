693809a329ba855ebac05fd9d8b1833a407596f4
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2015-12-04 12:07:05 +0100

    add posts to index page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
-	class PostsController < ApplicationController
-		def index
-		end
+class PostsController < ApplicationController
+	def index
+		@posts = Post.all
+	end
 	
 	def new
 		@post = Post.new
